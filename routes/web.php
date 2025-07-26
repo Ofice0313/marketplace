036ccef135c8bcb8bc775c56c8e597302a12389b
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-07-26 17:30:36 +0200

    Limpando as rotas e deixando-as organizadas

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-Route::get('/admin/stores', [StoreController::class, 'index']);
-
-
-Route::get('/admin/stores/create', [StoreController::class, 'create']);
-Route::post('/admin/stores/store', [StoreController::class, 'store']);
-
-Route::prefix('admin/stores')->group(function () {
+Route::prefix('admin/stores')->namespace('Admin')->group(function () {
     Route::get('/', [StoreController::class, 'index']);
     Route::get('/create', [StoreController::class, 'create']);
     Route::post('/store', [StoreController::class, 'store']);
